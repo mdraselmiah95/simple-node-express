b6d5d8084a9153c370970df6bff7a486a1612a62
rasel <russelroy15@gmail.com>
2021-10-23 19:43:10 +0600

    WIP on node express project

--- index.js
+++ index.js
@@ -14,16 +14,61 @@ app.get("/", (req, res) => {
 });
 
 const users = [
-  { id: 0, name: "Natalie Portman", born: "June 9, 1981" },
-  { id: 1, name: "Scarlett Johansson", born: "November 22, 1984" },
-  { id: 2, name: "Jennifer Lawrence", born: " August 15, 1990" },
-  { id: 3, name: "Emma Stone", born: "November 6, 1988" },
-  { id: 4, name: "Gal Gadot", born: "30 April 1985" },
-  { id: 5, name: "Alexandra Daddario", born: "March 16, 1986" },
-  { id: 6, name: "Megan Fox", born: "May 16, 1986" },
-  { id: 7, name: "Margot Robbie", born: "July 2, 1990" },
-  { id: 8, name: "Jessica Alba", born: " April 28, 1981" },
-  { id: 9, name: " Léa Seydoux", born: "1 July 1985" },
+  {
+    id: 0,
+    name: "Natalie Portman",
+    born: "June 9, 1981",
+    email: "natalie@gmial.com",
+  },
+  {
+    id: 1,
+    name: "Scarlett Johansson",
+    born: "November 22, 1984",
+    email: "scarlett@gmail.com",
+  },
+  {
+    id: 2,
+    name: "Jennifer Lawrence",
+    born: " August 15, 1990",
+    email: "jennifer@gmail.com",
+  },
+  {
+    id: 3,
+    name: "Emma Stone",
+    born: "November 6, 1988",
+    email: "emma@gmail.com",
+  },
+  { id: 4, name: "Gal Gadot", born: "30 April 1985", email: "gadot@gmail.com" },
+  {
+    id: 5,
+    name: "Alexandra Daddario",
+    born: "March 16, 1986",
+    email: "alexandra@gmail.com",
+  },
+  {
+    id: 6,
+    name: "Megan Fox",
+    born: "May 16, 1986",
+    email: "megan@gmail.com",
+  },
+  {
+    id: 7,
+    name: "Margot Robbie",
+    born: "July 2, 1990",
+    email: "margot@gmail.com",
+  },
+  {
+    id: 8,
+    name: "Jessica Alba",
+    born: " April 28, 1981",
+    email: "jessica@gmail.com",
+  },
+  {
+    id: 9,
+    name: " Léa Seydoux",
+    born: "1 July 1985",
+    email: "seydoux@gmail.com",
+  },
 ];
 
 app.get("/users", (req, res) => {
@@ -40,11 +85,12 @@ app.get("/users", (req, res) => {
 
 //app.METHOD
 app.post("/users", (req, res) => {
-  const newUser = req.body;
-  newUser.id = users.length;
-  users.push(newUser);
+  const newUsers = req.body;
+  newUsers.id = users.length;
+  users.push(newUsers);
   const newUser = console.log("Hitting the post.", req.body);
   // res.send(JSON.stringify(newUser));
+  res.json(newUsers);
 });
 
 //dynamic api
