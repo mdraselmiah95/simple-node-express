609eba869e10b4b841787b9aea693da7ba7c7ea3
rasel <russelroy15@gmail.com>
2021-10-23 15:22:19 +0600

    WIP on simple node express project

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send("This is my simple node express project.");
+  res.send("This is my simple node express project. nice");
 });
 
 app.listen(port, () => {
